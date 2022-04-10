b15fd762670b791ff79780695d33666a5abf4868
DK <94187390+justcallmedk@users.noreply.github.com>
2022-04-09 20:30:49 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+   //this is dumbest shit ever for not respecting gitignore in the first commit
+   aws : {
+     region : 'us-east-1',
+     user_identity_pool_id : 'fill in',
+     admin_identity_pool_id : 'fill in'
+   },
+   sp500 : {
+     url : 'https://datahub.io/core/s-and-p-500-companies/r/constituents.json'
+   },
+   auth : {
+     google : {
+       clientId : 'fill in'
+     }
+   }
+ };
