7c2d3f34a36d35dba2cdb76f0613c0ccedcb0fa4
DK <hi@justcallme.dk>
2022-04-09 20:16:11 -0400

    testing gitignore

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
   //this is dumbest shit ever for not respecting gitignore in the first commit
   aws : {
     region : 'us-east-1',
-    user_identity_pool_id : 'fill in',
-    admin_identity_pool_id : 'fill in'
+    user_identity_pool_id : 'fill in2',
+    admin_identity_pool_id : 'fill in2'
   },
   sp500 : {
     url : 'https://datahub.io/core/s-and-p-500-companies/r/constituents.json'
